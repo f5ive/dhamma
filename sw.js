d751d5e6bf4a2c6cd1e6b8476978b355274ceca5
f5ive <50686860+f5ive@users.noreply.github.com>
2026-09-05 12:03:55 +0530

    brand + install prompt: sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "dhammabell-v2";
-const SHELL = ["./", "index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png"];
+const CACHE = "dhammabell-v3";
+const SHELL = ["./", "index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png", "icon-maskable-512.png", "apple-touch-icon.png", "favicon.svg", "favicon.ico"];
 
 self.addEventListener("install", e => {
   e.waitUntil(
